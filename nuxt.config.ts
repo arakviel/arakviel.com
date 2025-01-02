05bd96fd9c85409a31da5b3442896c93daf2a23d
Vereshchahin Olexander <insider.arakviel@gmail.com>
2025-01-02 21:15:04 +0200

    new android lessons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,7 +25,8 @@ export default defineNuxtConfig({
                 'java',
                 'xml',
                 'c#',
-                'kotlin'
+                'kotlin',
+                'sql',
             ]
         }
     }
